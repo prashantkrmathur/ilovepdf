c9b58ce46bdcbd49c7dc727967e59b90db8c271f
Prashant Kumar <prashantnawa123@gmail.com>
2024-09-24 05:50:05 +0530

    css fix

--- ilovepdf-frontend/src/components/UI/HeroSection/HeroSection.jsx
+++ ilovepdf-frontend/src/components/UI/HeroSection/HeroSection.jsx
@@ -8,9 +8,9 @@ const HeroSection = () => {
         <div className="herosection">
         <div className="hero-toptext">
 
-            <h1 className='poppins-bold'>Every tool you need to work with PDFs in one place</h1>
+            <h1 className='poppins-bold' style={{fontSize: "24px", lineHeight: "34px"}}>Every tool you need to work with PDFs in one place</h1>
             <div className="para poppins-medium">
-            <p>Every tool you need to use PDFs, at your fingertips. All are 100% FREE and easy to use! Merge, split, compress, convert, rotate, unlock and watermark PDFs with just a few clicks.</p>
+            <p style={{fontSize: "18px", lineHeight: "28px"}}>Every tool you need to use PDFs, at your fingertips. All are 100% FREE and easy to use! Merge, split, compress, convert, rotate, unlock and watermark PDFs with just a few clicks.</p>
             </div>
         </div>
         <div className="cards-container poppins-medium">
